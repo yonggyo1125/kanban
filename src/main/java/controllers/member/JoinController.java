2c874ffb30dce0274d93544aa8b7daac750b261f
yonggyo1125 <yonggyo00@kakao.com>
2023-08-07 20:30:33 +0900

    수업 소스

--- src/main/java/controllers/member/JoinController.java
+++ src/main/java/controllers/member/JoinController.java
@@ -1,12 +1,14 @@
 package controllers.member;
 
+import commons.ViewUtils;
 import controllers.Controller;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 public class JoinController implements Controller {
     public void get(HttpServletRequest req, HttpServletResponse resp) {
-        System.out.println("회원 가입 - GET");
+
+        ViewUtils.load(req, resp, "member", "join");
     }
 
     public void post(HttpServletRequest req, HttpServletResponse resp) {
