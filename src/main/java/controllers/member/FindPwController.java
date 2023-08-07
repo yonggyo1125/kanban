cfdf76a9c9c9d25e4557b01c6144594c1e80664f
yonggyo1125 <yonggyo00@kakao.com>
2023-08-07 20:46:52 +0900

    수업 소스

--- src/main/java/controllers/member/FindPwController.java
+++ src/main/java/controllers/member/FindPwController.java
@@ -1,4 +1,19 @@
 package controllers.member;
 
-public class FindPwController {
+import commons.ViewUtils;
+import controllers.Controller;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class FindPwController implements Controller {
+    @Override
+    public void get(HttpServletRequest req, HttpServletResponse resp) {
+
+        ViewUtils.load(req, resp, "member", "find_pw");
+    }
+
+    @Override
+    public void post(HttpServletRequest req, HttpServletResponse resp) {
+
+    }
 }
