8b236097857c3a53248eb0fb6c37f091cf7db30b
yonggyo1125 <yonggyo00@kakao.com>
2023-08-07 21:49:32 +0900

    수업 소스

--- src/main/java/models/works/DeleteService.java
+++ src/main/java/models/works/DeleteService.java
@@ -3,4 +3,9 @@ package models.works;
 public class DeleteService {
     private WorkDao workDao = new WorkDao();
 
+    public void delete(long workNo) {
+
+        boolean result = workDao.delete(workNo);
+
+    }
 }
