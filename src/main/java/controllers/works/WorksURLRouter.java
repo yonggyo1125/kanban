416cc4ae721c5391f0ae5f1118286bcfb3b230c4
yonggyo1125 <yonggyo00@kakao.com>
2023-08-07 19:10:46 +0900

    작업 내용 정리

--- src/main/java/controllers/works/WorksURLRouter.java
+++ src/main/java/controllers/works/WorksURLRouter.java
@@ -4,6 +4,13 @@ import controllers.URLRouter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+/**
+ * 작업목록 - /works
+ * 작업등록 - /works/add
+ * 작업조회 - /works/작업번호
+ * 작업수정 - /works/edit/등록번호
+ * 작업삭제 - /works/delete/등록번호
+ */
 public class WorksURLRouter implements URLRouter {
     @Override
     public void route(HttpServletRequest req, HttpServletResponse resp, String location) {
