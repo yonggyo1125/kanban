513873a99d17f664872bf25fbfe1a914c7ddca18
yonggyo1125 <yonggyo00@kakao.com>
2023-08-07 20:24:42 +0900

    수업 소스

---
+++ src/main/java/commons/ViewUtils.java
@@ -0,0 +1,10 @@
+package commons;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class ViewUtils {
+    public static void load(HttpServletRequest req, HttpServletResponse resp, String dir, String fileName) {
+
+    }
+}
--- src/main/java/controllers/member/LoginController.java
+++ src/main/java/controllers/member/LoginController.java
@@ -1,12 +1,18 @@
 package controllers.member;
 
 import controllers.Controller;
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import java.io.IOException;
+
 public class LoginController implements Controller  {
     public void get(HttpServletRequest req, HttpServletResponse resp) {
-        System.out.println("로그인 - GET");
+        try {
+            RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/view/member/login.jsp");
+            rd.forward(req, resp);
+        } catch (Exception e) {}
     }
 
     public void post(HttpServletRequest req, HttpServletResponse resp) {
