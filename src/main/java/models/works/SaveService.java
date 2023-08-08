39ca6ef58f175a8b86f86170c65fefa082c4b9e1
yonggyo1125 <yonggyo00@kakao.com>
2023-08-08 19:51:08 +0900

    수업 소스

--- src/main/java/models/works/SaveService.java
+++ src/main/java/models/works/SaveService.java
@@ -9,8 +9,19 @@ public class SaveService {
 
 
     public void save(Work work) {
+        /** 필수 항목 검증 - status, subject, content */
+        if (work.getStatus() == null) {
+            throw new WorkValidationException("작업 상태를 선택하세요.");
+        }
+        String subject = work.getSubject();
+        if (subject == null || subject.isBlank()) {
+            throw new WorkValidationException("작업 제목을 입력하세요.");
+        }
 
-        // work 데이터 검증 (유효성 검사)
+        String content = work.getContent();
+        if (content == null || content.isBlank()) {
+            throw new WorkValidationException("작업 내용을 입력하세요.");
+        }
 
         workDao.save(work); // 의존
 
