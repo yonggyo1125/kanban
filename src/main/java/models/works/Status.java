eb7e20f9667948385922f0a7de7495151d910c37
yonggyo1125 <yonggyo00@kakao.com>
2023-08-08 21:19:01 +0900

    수업 소스

--- src/main/java/models/works/Status.java
+++ src/main/java/models/works/Status.java
@@ -1,8 +1,22 @@
 package models.works;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public enum Status {
-    READY,
-    PROGRESS,
-    DONE,
-    POSTPONE
+    READY("준비중"),
+    PROGRESS("진행중"),
+    DONE("완료"),
+    POSTPONE("보류");
+
+    private String status;
+
+    Status(String status) {
+        this.status = status;
+    }
+
+    public List<String[]> getList() {
+        List<String[]> items = new ArrayList<>();
+
+    }
 }
