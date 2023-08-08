5233db3261c818ec48d4b96196870410d361d1cd
yonggyo1125 <yonggyo00@kakao.com>
2023-08-08 20:34:53 +0900

    수업 소스

--- src/main/java/models/works/InfoService.java
+++ src/main/java/models/works/InfoService.java
@@ -1,5 +1,9 @@
 package models.works;
 
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class InfoService {
     private WorkDao workDao;
 
@@ -12,5 +16,15 @@ public class InfoService {
         Work work = workDao.get(workNo);
 
         return work;
+    }
+
+    public Map<Status, List<Work>> getList(Work work) {
+
+        List<Work> items = workDao.gets(work);
+        if (items == null) return null;
+
+        Map<Status, List<Work>> data = items.stream().collect(Collectors.groupingBy(Work::getStatus));
+
+        return data;
     }
 }
