90b34d00738c7ad585dd16b2653f664405b18cbb
yonggyo1125 <yonggyo00@kakao.com>
2023-08-09 20:30:50 +0900

    수업 소스

--- src/main/java/controllers/works/ViewController.java
+++ src/main/java/controllers/works/ViewController.java
@@ -4,10 +4,32 @@ import commons.ViewUtils;
 import controllers.Controller;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import models.works.InfoService;
+import models.works.Work;
+import models.works.WorkNotFoundException;
+import models.works.WorkServiceManager;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static commons.ScriptUtils.alertError;
 
 public class ViewController implements Controller {
     @Override
     public void get(HttpServletRequest req, HttpServletResponse resp) {
+
+        try {
+            InfoService infoService = WorkServiceManager.getInstance().infoService();
+            Work work = infoService.get(getWorkNo(req));
+            if (work == null) {
+                throw new WorkNotFoundException();
+            }
+
+            req.setAttribute("work", work);
+
+        } catch (Exception e) {
+            alertError(resp, e, -1);
+        }
 
         ViewUtils.load(req, resp, "works", "view");
     }
@@ -15,5 +37,16 @@ public class ViewController implements Controller {
     @Override
     public void post(HttpServletRequest req, HttpServletResponse resp) {
 
+    }
+
+    private long getWorkNo(HttpServletRequest req) {
+        String pattern = "works/(\\d*)";
+        Pattern p = Pattern.compile(pattern);
+        Matcher matcher = p.matcher(req.getRequestURI());
+        if (matcher.find()) {
+            return Long.parseLong(matcher.group(1));
+        }
+
+        return 0L;
     }
 }
