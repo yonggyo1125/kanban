d5ca282d29d0eef0d036afcabf493aaa6bda2449
yonggyo1125 <yonggyo00@kakao.com>
2023-08-09 19:47:16 +0900

    수업 소스

--- src/main/java/controllers/works/SaveController.java
+++ src/main/java/controllers/works/SaveController.java
@@ -8,12 +8,18 @@ import jakarta.servlet.http.HttpServletResponse;
 import models.works.SaveService;
 import models.works.WorkServiceManager;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 
 public class SaveController implements Controller {
     @Override
     public void get(HttpServletRequest req, HttpServletResponse resp) {
         String URI = req.getRequestURI();
         String mode = URI.indexOf("edit") != -1 ? "edit" : "add";
+
+        long workNo = getWorkNo(req);
+        System.out.println(workNo);
 
         String[] addScript = { "ckeditor/ckeditor", "work/form" };
         req.setAttribute("addScript", addScript);
@@ -26,12 +32,32 @@ public class SaveController implements Controller {
         SaveService saveService = WorkServiceManager.getInstance().saveService();
         try {
             saveService.save(req);
-
-            resp.sendRedirect(req.getContextPath() + "/works"); // Location: /kanban/works
+            
+            // 추가, 수정 성공시 목록으로 이동
+            go(resp, req.getContextPath() + "/works", "parent");
 
         } catch (Exception e) {
             alertError(resp, e);
             e.printStackTrace();
         }
+    }
+
+    /**
+     * 작업 번호 추출
+     *
+     * @param req
+     * @return
+     */
+    private long getWorkNo(HttpServletRequest req) {
+        String URI = req.getRequestURI();
+
+        String pattern = "edit/(\\d*)";
+        Pattern p = Pattern.compile(pattern);
+        Matcher matcher = p.matcher(URI);
+        if (matcher.find()) {
+            return Long.parseLong(matcher.group(1));
+        }
+
+        return 0L;
     }
 }
