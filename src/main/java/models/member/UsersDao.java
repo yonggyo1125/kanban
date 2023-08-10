eba07589bf580cd79184e05003fd0e868667c94b
yonggyo1125 <yonggyo00@kakao.com>
2023-08-10 20:50:56 +0900

    수업 소스

--- src/main/java/models/member/UsersDao.java
+++ src/main/java/models/member/UsersDao.java
@@ -2,16 +2,29 @@ package models.member;
 
 import configs.DBConnection;
 import org.apache.ibatis.session.SqlSession;
+import org.mindrot.jbcrypt.BCrypt;
 
 public class UsersDao {
     public boolean register(Users users) {
         SqlSession sqlSession = DBConnection.getSession();
+        String hash = BCrypt.hashpw(users.getUserPw(), BCrypt.gensalt(12));
+        users.setUserPw(hash);
+
+        int affectedRows = sqlSession.insert("UserMapper.add", users);
 
         sqlSession.commit();
+
+        return affectedRows > 0;
     }
 
     public Users get(String userId) {
+        SqlSession sqlSession = DBConnection.getSession();
+        Users params = new Users();
+        params.setUserId(userId);
 
+        Users users = sqlSession.selectOne("UserMapper.info", params);
+
+        return users;
     }
 
     public boolean exists(String userId) {
