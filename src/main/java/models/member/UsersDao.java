1480201de78b21e4fb3829097dbedb296a8dd4b2
yonggyo1125 <yonggyo00@kakao.com>
2023-08-10 20:46:07 +0900

    수업 소스

--- src/main/java/models/member/UsersDao.java
+++ src/main/java/models/member/UsersDao.java
@@ -1,4 +1,20 @@
 package models.member;
 
+import configs.DBConnection;
+import org.apache.ibatis.session.SqlSession;
+
 public class UsersDao {
+    public boolean register(Users users) {
+        SqlSession sqlSession = DBConnection.getSession();
+
+        sqlSession.commit();
+    }
+
+    public Users get(String userId) {
+
+    }
+
+    public boolean exists(String userId) {
+
+    }
 }
