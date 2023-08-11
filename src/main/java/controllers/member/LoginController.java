2c46b07e448faa3593f3544e4e6cc4bfb3f5002a
yonggyo1125 <yonggyo00@kakao.com>
2023-08-11 21:44:34 +0900

    수업 소스

--- src/main/java/controllers/member/LoginController.java
+++ src/main/java/controllers/member/LoginController.java
@@ -5,8 +5,13 @@ import controllers.Controller;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import models.member.LoginService;
+import models.member.MemberServiceManager;
 
 import java.io.IOException;
+
+import static commons.ScriptUtils.alertError;
+import static commons.ScriptUtils.go;
 
 public class LoginController implements Controller  {
     public void get(HttpServletRequest req, HttpServletResponse resp) {
@@ -16,6 +21,15 @@ public class LoginController implements Controller  {
     }
 
     public void post(HttpServletRequest req, HttpServletResponse resp) {
+        try{
+            LoginService loginService = MemberServiceManager.getInstance().loginService();
+            loginService.login(req);
 
+            // 로그인 성공 -> 작업 목록 이동
+            go(resp, req.getContextPath() + "/works", "parent");
+
+        } catch (Exception e) {
+            alertError(resp, e);
+        }
     }
 }
