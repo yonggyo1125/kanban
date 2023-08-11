643adae5afa7580c4ee9a4a91a4b0355ab8b7279
yonggyo1125 <yonggyo00@kakao.com>
2023-08-11 21:35:40 +0900

    수업 소스

--- src/main/java/models/member/LoginValidator.java
+++ src/main/java/models/member/LoginValidator.java
@@ -13,11 +13,26 @@ public class LoginValidator implements Validator<HttpServletRequest>, RequiredVa
     }
 
     @Override
-    public void check(HttpServletRequest httpServletRequest) {
+    public void check(HttpServletRequest request) {
         /**
          * 1. 필수 항목 검증(userId, userPw)
          * 2. userId로 회원이 조회 되는지 체크
          * 3. 비밀번호 검증
          */
+
+        String userId = request.getParameter("userId");
+        String userPw = request.getParameter("userPw");
+
+        // 1. 필수 항목 검증(userId, userPw)
+        checkRequired(userId, new LoginValidationException("아이디를 입력하세요."));
+        checkRequired(userPw, new LoginValidationException("비밀번호를 입력하세요."));
+
+        String message = "아이디 또는 비밀번호가 일치하지 않습니다.";
+
+        // 2. userId로 회원이 조회 되는지 체크
+        Users users = usersDao.get(userId);
+        if (users == null) {
+            throw new LoginValidationException(message);
+        }
     }
 }
