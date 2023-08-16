a4c9fbb4984481d263dd26a19b0f33195bc50835
yonggyo1125 <yonggyo00@kakao.com>
2023-08-16 19:15:29 +0900

    수업 소스

--- src/main/java/models/works/Work.java
+++ src/main/java/models/works/Work.java
@@ -8,6 +8,7 @@ import java.time.LocalDateTime;
 public class Work {
     private long workNo;
     private long userNo;
+    private String userNm;
     private Status status = Status.READY;
     private String subject;
     private String content;
