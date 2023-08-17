63a58c3bbb23ef3148fc90273c3d86332f6cb04f
yonggyo1125 <yonggyo00@kakao.com>
2023-08-17 21:11:42 +0900

    수업 소스

--- src/main/java/models/files/FileServiceManager.java
+++ src/main/java/models/files/FileServiceManager.java
@@ -1,6 +1,19 @@
 package models.files;
 
 public class FileServiceManager {
+
+    private static FileServiceManager instance;
+
+    private FileServiceManager() {}
+
+    public static FileServiceManager getInstance() {
+        if (instance == null) {
+            instance = new FileServiceManager();
+        }
+
+        return instance;
+    }
+
     public FileInfoDao fileInfoDao() {
         return new FileInfoDao();
     }
