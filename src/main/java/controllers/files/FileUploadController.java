735df5d294e62cb131f1f77deef835854954c664
yonggyo1125 <yonggyo00@kakao.com>
2023-08-17 21:40:30 +0900

    수업 소스

--- src/main/java/controllers/files/FileUploadController.java
+++ src/main/java/controllers/files/FileUploadController.java
@@ -5,29 +5,17 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.apache.commons.fileupload2.core.DiskFileItem;
-import org.apache.commons.fileupload2.jakarta.JakartaServletDiskFileUpload;
+import models.files.FileServiceManager;
+import models.files.FileUploadService;
 
 import java.io.IOException;
-import java.nio.file.Path;
-import java.util.List;
 
 @WebServlet("/file/upload")
 public class FileUploadController extends HttpServlet {
     @Override
     public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String uploadPath = req.getServletContext().getRealPath("/static/uploads");
+        FileUploadService uploadService = FileServiceManager.getInstance().fileUploadService();
 
-        JakartaServletDiskFileUpload upload = new JakartaServletDiskFileUpload();
-        upload.setSizeMax(1024 * 1024 * 20); // 20mb
-
-        List<DiskFileItem> items = upload.parseRequest(req);
-        for (DiskFileItem item : items) {
-            if (!item.isFormField()) { // 파일 데이터
-                String fileName = item.getName();
-                Path path = Path.of(uploadPath, fileName);
-                item.write(path);
-            }
-        }
+        uploadService.uploads(req);
     }
 }
--- src/main/java/models/files/FileUploadService.java
+++ src/main/java/models/files/FileUploadService.java
@@ -1,9 +1,15 @@
 package models.files;
 
+import commons.MemberUtil;
 import jakarta.servlet.http.HttpServletRequest;
-import org.apache.commons.fileupload2.core.FileItem;
+import org.apache.commons.fileupload2.core.DiskFileItem;
+import org.apache.commons.fileupload2.jakarta.JakartaServletDiskFileUpload;
 
-import java.util.List;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Path;
+import java.util.*;
 
 public class FileUploadService {
     private FileInfoDao fileInfoDao;
@@ -12,8 +18,68 @@ public class FileUploadService {
         this.fileInfoDao = fileInfoDao;
     }
 
-    public List<FileInfo> uploads(HttpServletRequest request) {
+    public List<FileInfo> uploads(HttpServletRequest request) throws IOException {
 
-        return null;
+        long userNo = MemberUtil.isLogin(request) ? MemberUtil.getUser(request).getUserNo() : 0;
+
+        String uploadPath = request.getServletContext().getRealPath("/static/uploads");
+
+        JakartaServletDiskFileUpload upload = new JakartaServletDiskFileUpload();
+        upload.setSizeMax(1024 * 1024 * 20);
+
+        List<DiskFileItem> items = upload.parseRequest(request);
+        // 일반 양식 데이터 처리 S
+        Map<String, String> params = new HashMap<>();
+        for(DiskFileItem item : items) {
+            if (item.isFormField()) { // 일반 양식 데이터
+                String name = item.getFieldName();
+                String value = item.getString(Charset.forName("UTF-8"));
+                params.put(name, value);
+            }
+        }
+        // 일반 양식 데이터 처리 E
+
+        // 파일 업로드 처리 S
+        String gid = params.get("gid");
+        gid = gid == null || gid.isBlank() ? UUID.randomUUID().toString() : gid;
+
+        String location = params.get("location");
+
+        List<FileInfo> uploadedFiles = new ArrayList<>(); // 업로드 성공 파일 목록
+        for (DiskFileItem item : items) {
+            if (!item.isFormField()) {
+                String fileName = item.getName();
+                String fileType = item.getContentType();
+                String extension = fileName.substring(fileName.lastIndexOf(".") + 1);
+
+                FileInfo fileInfo = new FileInfo();
+                fileInfo.setGid(gid);
+                fileInfo.setLocation(location);
+                fileInfo.setFileName(fileName);
+                fileInfo.setFileType(fileType);
+                fileInfo.setExtension(extension);
+                fileInfo.setUserNo(userNo);
+
+                if (!fileInfoDao.add(fileInfo)) { // 파일 정보 저장 실패
+                    long id = fileInfo.getId();
+
+                    File dir = new File(uploadPath + "/" + (id % 10));
+                    if (!dir.exists()) { // 디렉토리가 없는 경우
+                        dir.mkdir();
+                    }
+
+                    String uploadFileName = id + "." + extension;
+                    File file = new File(dir, uploadFileName);
+                    Path path = Path.of(file.getPath());
+                    item.write(path);
+
+                    uploadedFiles.add(fileInfo);
+                }
+            }
+        }
+
+        // 파일 업로드 처리 E
+
+        return uploadedFiles;
     }
 }
